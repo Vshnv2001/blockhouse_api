5bfb4fbd39e847d9bdcc6155c7392e1c30ecb3d2
Vshnv2001 <mvaishnav.sg@gmail.com>
2025-02-25 03:49:09 +0800

    Second Modif

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ def get_db():
 
 @app.get('/') # get method to empty endpoint
 def first_response():
-    return {"response": "first modification"}
+    return {"response": "second modification"}
 
 @app.get('/orders')
 def get_orders(db: Session = Depends(get_db)):
