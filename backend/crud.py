8b6bbb999e8eef8231803d2973ea59a64fb35c0d
Vshnv2001 <mvaishnav.sg@gmail.com>
2025-02-25 00:52:08 +0800

    Sample commit to test ci

--- backend/crud.py
+++ backend/crud.py
@@ -5,6 +5,7 @@ import datetime
 def get_trade_orders(db: Session):
     return db.query(models.TradeOrders).all()
 
+
 def add_trade_order(db: Session, trade_order: schemas.TradeOrder):
     new_order = models.TradeOrders(symbol=trade_order.symbol, price=trade_order.price, quantity=trade_order.quantity, order_type=trade_order.order_type, date=datetime.datetime.now())
     db.add(new_order)
